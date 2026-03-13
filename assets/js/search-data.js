56da2629db4e66bb4f535298e11473208fc9c1fd
kwade4 <48563808+kwade4@users.noreply.github.com>
2026-03-13 19:26:29 +0000

    Deploying to gh-pages from @ kwade4/kwade4.github.io@53610bde60913b9988c42167c71eb8f7a9087668 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,18 +42,18 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-awarded-the-cihr-doctoral-scholarship",
+          title: '🎉 Awarded the CIHR Doctoral Scholarship!',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-successfully-passed-my-thesis-proposal-defense-tsp",
+          title: '✅ Successfully passed my Thesis Proposal Defense (TSP)!',
+          description: "",
+          section: "News",},{id: "news-invited-speaker-at-the-canadian-undergraduate-conference-in-ai-cucai-in-toronto",
+          title: 'Invited Speaker at the Canadian Undergraduate Conference in AI (CUCAI) in Toronto.',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-raise-dri",
+            },},{id: "projects-raise-dri",
           title: 'RAISE-DRI',
           description: "Research and AI at Scale",
           section: "Projects",handler: () => {
